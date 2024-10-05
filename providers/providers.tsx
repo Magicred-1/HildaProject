82c1cfd292c973f57cc779ececbd99b5de82859d
Magicred-1 <djason.gadiou.sio@gmail.com>
2024-10-05 02:43:31 +0200

    Layout fix

--- providers/providers.tsx
+++ providers/providers.tsx
@@ -2,21 +2,42 @@
 
 import { useRouter } from "next/navigation";
 
-import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { DynamicContextProvider, mergeNetworks } from "@dynamic-labs/sdk-react-core";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import { ZeroDevSmartWalletConnectors } from "@dynamic-labs/ethereum-aa";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
 
+  const evmNetworks = [
+  {
+      blockExplorerUrls: ['https://testnet.snowtrace.io/'],
+      chainId: 43113,
+      chainName: 'Avalanche Fuji Testnet',
+      iconUrls: ['https://app.dynamic.xyz/assets/networks/avax.svg'],
+      name: 'Avalanche Fuji Testnet',
+      nativeCurrency: {
+        decimals: 18,
+        name: 'AVAX',
+        symbol: 'AVAX',
+      },
+      networkId: 43113,
+      rpcUrls: ['https://avalanche-fuji-c-chain-rpc.publicnode.com'],
+      vanityName: 'Avalanche Fuji Testnet',
+    },
+  ];
+
   return (
     <DynamicContextProvider
       settings={{
-        environmentId: "832a5743-3046-429c-b89d-8dd0b3496f9d",
+        environmentId: "6d869255-8971-474a-bf98-826918ac83e9",
         walletConnectors: [
           EthereumWalletConnectors,
           ZeroDevSmartWalletConnectors,
         ],
+        overrides: {
+          evmNetworks: (networks) => mergeNetworks(evmNetworks, networks),
+        },
         events: {
           onAuthSuccess: () => {
             console.log("Auth success");
