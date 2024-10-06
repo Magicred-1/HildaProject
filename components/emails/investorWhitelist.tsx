b95cd02286d20f5367562a53aa41ef083a87e87d
Magicred-1 <djason.gadiou.sio@gmail.com>
2024-10-06 12:30:42 +0200

    Route Resend

--- components/emails/investorWhitelist.tsx
+++ components/emails/investorWhitelist.tsx
@@ -10,6 +10,6 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
     tokenSymbol,
 }) => (
   <div>
-    <h1>Hello {email} you have been added to the whitelist for {tokenSymbol} on Hilda Project</h1>
+    <h1>Hello, you have been added to the whitelist for ${tokenSymbol} check it out in <a href="https://hildaproject.xyz/buy-bond">Hilda</a></h1>
   </div>
 );
